5e73202054aec97f25b8c58a467f564cf8ee4944
angel david <angel.36b@gmail.com>
2026-05-14 21:37:38 +0300

    We make histograma and calculate the proportion

--- exercise.py
+++ exercise.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('adult_data_train.csv')
 
@@ -10,9 +11,28 @@ print(df_cleaned.head())
 #we get the types of all the collumns/ получаем типы всех столбцов
 data_types = df_cleaned.dtypes
 #we count how many are type object and numeric / подсчитаем, склоько из них имеют тип object
-non_numeric_count = (data_types == 'object').sum()
+non_numeric_cols = df_cleaned.select_dtypes(include=['object']).columns
+non_numeric_count = len(non_numeric_cols)
 
 numeric_count = (data_types == 'int64').sum() + (data_types == 'float64').sum()
 
 print(f"object type:{non_numeric_count}")
 print(f"numeric type:{numeric_count}")
+
+ax = df['label'].value_counts().plot(kind='bar', color=['skyblue', 'salmon'] ,edgecolor='black')
+#construct the histograma of the class distribution
+#label is the column that contains the classes (0 and 1)
+ax.bar_label(ax.containers[0], label_type='edge', padding=3)
+
+plt.title('Distribution of Objects by class')
+plt.xlabel('class (0 or 1)')
+plt.ylabel('Number of Objects')
+plt.xticks(rotation=0)
+plt.ylim(0, df['label'].value_counts().max()*1.1)
+plt.show()
+#3. Calculate the proportion of objects in class 0
+proportions = df['label'].value_counts(normalize=True)
+
+
+class_0_proportion =round(proportions[0], 3)
+print(f"Proportion of objects in class 0:{class_0_proportion}")
