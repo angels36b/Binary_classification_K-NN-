0d44db1f7382751e1a8b60ca780445df25aef335
angel david <angel.36b@gmail.com>
2026-05-14 18:33:38 +0300

    we get the type of all collumns

--- exercise.py
+++ exercise.py
@@ -6,3 +6,13 @@ df = pd.read_csv('adult_data_train.csv')
 #delate columns education - marital
 df_cleaned = df.drop(columns=['education', 'marital-status'])
 print(df_cleaned.head())
+
+#we get the types of all the collumns/ получаем типы всех столбцов
+data_types = df_cleaned.dtypes
+#we count how many are type object and numeric / подсчитаем, склоько из них имеют тип object
+non_numeric_count = (data_types == 'object').sum()
+
+numeric_count = (data_types == 'int64').sum() + (data_types == 'float64').sum()
+
+print(f"object type:{non_numeric_count}")
+print(f"numeric type:{numeric_count}")
