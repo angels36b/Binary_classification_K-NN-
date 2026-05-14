4ce4eabd904862848245d93a5a6eae2e2e8b2573
angel david <angel.36b@gmail.com>
2026-05-14 22:05:16 +0300

    Test F1, KneighborsClassifier

--- exercise.py
+++ exercise.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import f1_score
 
 df = pd.read_csv('adult_data_train.csv')
 
@@ -36,3 +39,30 @@ proportions = df['label'].value_counts(normalize=True)
 
 class_0_proportion =round(proportions[0], 3)
 print(f"Proportion of objects in class 0:{class_0_proportion}")
+
+numeric_cols=df.select_dtypes(include=['number']).columns
+df_numeric = df[numeric_cols].copy()
+
+#Convert labels 1 and 0 to treu and False as requested
+df_numeric['label'] = df_numeric['label'].astype(bool)
+
+#Separate the features (X) from the target label
+X = df_numeric.drop(columns=['label'])
+y = df_numeric['label']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.20, random_state=53, stratify=y
+)
+
+#Find sample mean for the column fnlwgt of the obtained training dataset
+fnlwgt_mean = X_train['fnlwgt'].mean()
+print(f"Sample mean for fnlwgt (Train): {round(fnlwgt_mean,3)}")
+
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train) #Training the model
+
+#Evaluate on the test data
+y_pred = knn.predict(X_test)
+
+f1 = f1_score(y_test, y_pred)
+print(f"F1 Score for test dataset: {round(f1,3)}")
